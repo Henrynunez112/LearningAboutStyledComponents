a3948b768c341d6fa65f73b12f75f94bed7e62b3
Henrynunez112 <henrynunez@pursuit.org>
2020-03-31 14:01:45 -0400

    created a circle component to work on circles

--- intro-to-styled-comp/src/App.js
+++ intro-to-styled-comp/src/App.js
@@ -1,32 +1,46 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import styled, { css } from "styled-components";
 
 function App() {
   const Button = styled.button`
-  background: transparent;
-  border-radius: 3px;
-  border: 2px solid palevioletred;
-  color: palevioletred;
-  margin: 0 1em;
-  padding: 0.25em 1em;
-  ${props =>
-    props.primary &&
-    css`
-      background: palevioletred;
-      color: blue;
-    `}
-`;
+    background: transparent;
+    border-radius: 3px;
+    border: 2px solid palevioletred;
+    color: palevioletred;
+    margin: 0 1em;
+    padding: 0.25em 1em;
+    ${props =>
+      props.primary &&
+      css`
+        background: palevioletred;
+        color: white;
+      `};
+    ${props =>
+      props.secondary &&
+      css`
+        background: white;
+        color: blue;
+      `};
+    ${props =>
+      props.third &&
+      css`
+        background: black;
+        color: green;
+      `};
+  `;
 
-const Container = styled.div`
+  const Container = styled.div`
     text-align: center;
-`
+  `;
   return (
     <div className="App">
-    <Container>
-      <Button>Normal Button</Button>
-      <Button primary>Primary Button</Button>
-    </Container>
+      <Container>
+        <Button>Normal Button</Button>
+        <Button primary>Primary Button</Button>
+        <Button secondary>Secondary Button</Button>
+        <Button third>Thrid Button</Button>
+      </Container>
     </div>
   );
 }
